69396684691917585d7f20fbcec4e1934ef6e10b
PragadeeswarAP <praga.power@gmail.com>
2014-05-23 12:29:26 +0530

    initial commit

--- src/main/scala/scalding/examples/WordCountJob.scala
+++ src/main/scala/scalding/examples/WordCountJob.scala
@@ -7,21 +7,21 @@ class WordCountJob(args : Args) extends Job(args) {
     .flatMap('line -> 'word) {
       line : String => tokenize(line)
     }
-    .groupBy('word) { _.size }
+    .groupBy('word ) { group:GroupBuilder => group.size }
+    .filter('size){size : Int => size > 4}
     .write( Tsv( args("output") ) )
 
   // split a piece of text into individual words
-  def tokenize(text : String) : Array[String] = {
-    text.toLowerCase.split(" ")
+  def tokenize(text : String) : List[String] = {
+    List(text.toLowerCase.split("/")(2))
   }
 }
 
 object WordCountJob extends App {
   val progargs: Array[String] = List(
-    "-Dmapred.map.tasks=200",
-    "scalding.examples.WordCountJob",
-    "--input", "/tmp/files",
-    "--output", "/tmp/wordcount.txt",
+    "-Dmapred.map.tasks=200","scalding.examples.WordCountJob",
+    "--input", "/home/yooo/Documents/Indix/scalding/sampleurls",
+    "--output", "/home/yooo/Documents/Indix/Scalding/output",
     "--hdfs"
   ).toArray
   Tool.main(progargs)
